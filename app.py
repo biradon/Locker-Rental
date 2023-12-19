4a1537c35e2749adccd39f2cd991560207ab53a5
Minh Truong <61044654+biradon@users.noreply.github.com>
2023-12-18 23:04:23 -0500

    Many updates
    - Add C2 C4 C5
    - Add Favicon

--- app.py
+++ app.py
@@ -7,17 +7,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/C3', methods=['GET', 'POST'])
-def C3():
-    # Read the file and store in the array, after that paste to the HTML
-    lockers_C3 = []
-    with open('buildings/C3.txt', 'r') as file:
-        for line in file:
-            lockers_C3.append(line.strip('\n'))
-
-    if request.method == 'POST':
-        return redirect(url_for('index'))
-    return render_template('C3.html', lockers_C3=lockers_C3)
 
 @app.route('/C1', methods=['GET', 'POST'])
 def C1():
@@ -30,6 +19,55 @@ def C1():
     if request.method == 'POST':
         return redirect(url_for('index'))
     return render_template('C1.html', lockers_C1=lockers_C1)
+
+@app.route('/C2', methods=['GET', 'POST'])
+def C2():
+    # Read the file and store in the array, after that paste to the HTML
+    lockers_C2 = []
+    with open('buildings/C2.txt', 'r') as file:
+        for line in file:
+            lockers_C2.append(line.strip('\n'))
+
+    if request.method == 'POST':
+        return redirect(url_for('index'))
+    return render_template('C2.html', lockers_C2=lockers_C2)
+
+@app.route('/C3', methods=['GET', 'POST'])
+def C3():
+    # Read the file and store in the array, after that paste to the HTML
+    lockers_C3 = []
+    with open('buildings/C3.txt', 'r') as file:
+        for line in file:
+            lockers_C3.append(line.strip('\n'))
+
+    if request.method == 'POST':
+        return redirect(url_for('index'))
+    return render_template('C3.html', lockers_C3=lockers_C3)
+
+@app.route('/C4', methods=['GET', 'POST'])
+def C4():
+    # Read the file and store in the array, after that paste to the HTML
+    lockers_C4 = []
+    with open('buildings/C4.txt', 'r') as file:
+        for line in file:
+            lockers_C4.append(line.strip('\n'))
+
+    if request.method == 'POST':
+        return redirect(url_for('index'))
+    return render_template('C4.html', lockers_C4=lockers_C4)
+
+@app.route('/C5', methods=['GET', 'POST'])
+def C5():
+    # Read the file and store in the array, after that paste to the HTML
+    lockers_C5 = []
+    with open('buildings/C5.txt', 'r') as file:
+        for line in file:
+            lockers_C5.append(line.strip('\n'))
+
+    if request.method == 'POST':
+        return redirect(url_for('index'))
+    return render_template('C5.html', lockers_C5=lockers_C5)
+
 
 
 @app.route('/fetchdata', methods=['GET','POST'])
--- static/compare.js
+++ static/compare.js
@@ -16,6 +16,26 @@ function compareLockers(lockerFromFlask) {
             let container6 = document.getElementsByClassName('accordion-body-6')[0];
             let container7 = document.getElementsByClassName('accordion-body-7')[0];
             let container8 = document.getElementsByClassName('accordion-body-8')[0];
+            // Building C Floor 2
+            let container9 = document.getElementsByClassName('accordion-body-9')[0];
+            let container10 = document.getElementsByClassName('accordion-body-10')[0];
+            let container11 = document.getElementsByClassName('accordion-body-11')[0];
+            let container12 = document.getElementsByClassName('accordion-body-12')[0];
+            let container13 = document.getElementsByClassName('accordion-body-13')[0];
+            // Building C Floor 4
+            let container14 = document.getElementsByClassName('accordion-body-14')[0];
+            let container15 = document.getElementsByClassName('accordion-body-15')[0];
+            let container16 = document.getElementsByClassName('accordion-body-16')[0];
+            let container17 = document.getElementsByClassName('accordion-body-17')[0];
+            let container18 = document.getElementsByClassName('accordion-body-18')[0];
+            let container19 = document.getElementsByClassName('accordion-body-19')[0];
+            let container20 = document.getElementsByClassName('accordion-body-20')[0];
+            // Building C Floor 5
+            let container21 = document.getElementsByClassName('accordion-body-21')[0];
+            let container22 = document.getElementsByClassName('accordion-body-22')[0];
+            let container23 = document.getElementsByClassName('accordion-body-23')[0];
+            let container24 = document.getElementsByClassName('accordion-body-24')[0];
+            let container25 = document.getElementsByClassName('accordion-body-25')[0];
                         
             for (let i=0; i < availableLockers.length; i++)
             {
@@ -68,10 +88,112 @@ function compareLockers(lockerFromFlask) {
                         container8.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
                     }
                 }
+                if(building == "C2")
+                {
+                    let location = availableLockers[i][2];
+                    if (location == "0")
+                    {
+                        // Append the content of the locker to the container
+                        container9.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                    if (location == "1")
+                    {
+                        // Append the content of the locker to the container
+                        container10.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                    if (location == "2")
+                    {
+                        // Append the content of the locker to the container
+                        container11.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                    if (location == "3" || location == "8")
+                    {
+                        // Append the content of the locker to the container
+                        container12.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                    if (location == "4")
+                    {
+                        // Append the content of the locker to the container
+                        container13.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                }
+                if(building == "C4")
+                {
+                    let location = availableLockers[i][2];
+                    if (location == "0")
+                    {
+                        // Append the content of the locker to the container
+                        container14.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                    if (location == "1")
+                    {
+                        // Append the content of the locker to the container
+                        container15.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                    if (location == "2")
+                    {
+                        // Append the content of the locker to the container
+                        container16.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                    if (location == "5")
+                    {
+                        // Append the content of the locker to the container
+                        container17.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                    if (location == "6" || location == "7" )
+                    {
+                        // Append the content of the locker to the container
+                        container18.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                    if (location == "8")
+                    {
+                        // Append the content of the locker to the container
+                        container19.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                    if (location == "9")
+                    {
+                        // Append the content of the locker to the container
+                        container20.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                }
+                if(building == "C5")
+                {
+                    let location = availableLockers[i][2];
+                    if (location == "0")
+                    {
+                        // Append the content of the locker to the container
+                        container21.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                    if (location == "1")
+                    {
+                        // Append the content of the locker to the container
+                        container22.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                    if (location == "2" || location == "3")
+                    {
+                        // Append the content of the locker to the container
+                        container23.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                    if (location == "4")
+                    {
+                        // Append the content of the locker to the container
+                        container24.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                    if (location == "5")
+                    {
+                        // Append the content of the locker to the container
+                        container25.innerHTML += `<span style="font-size: 18px; font-weight: bold;">${availableLockers[i]}</span>, `;
+                    }
+                }
             }
             const containers = [
                 container1, container2, container3, container4,
-                container5, container6, container7, container8
+                container5, container6, container7, container8,
+                container9, container10, container11, container12,
+                container13, container14, container15, container16,
+                container17, container18, container19, container20,
+                container21, container22, container23, container24,
+                container25
             ];
             
             for (let container of containers) {
